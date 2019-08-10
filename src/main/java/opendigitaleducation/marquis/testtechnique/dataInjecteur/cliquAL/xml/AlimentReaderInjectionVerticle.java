73b083d1a40fdb3d91b37b378dba338c38f86de8
MARQUIS Vincent <vincent.marquis@seloger.com>
2019-08-10 10:48:38 +0200

    Unsed import remove

--- src/main/java/opendigitaleducation/marquis/testtechnique/dataInjecteur/cliquAL/xml/AlimentReaderInjectionVerticle.java
+++ src/main/java/opendigitaleducation/marquis/testtechnique/dataInjecteur/cliquAL/xml/AlimentReaderInjectionVerticle.java
@@ -1,6 +1,5 @@
 package opendigitaleducation.marquis.testtechnique.dataInjecteur.cliquAL.xml;
 
-import io.vertx.config.ConfigRetriever;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
 import io.vertx.core.eventbus.EventBus;
