2d43c6a6db63b11d727edfc633451f6f869d3c4a
MARQUIS Vincent <vincent.marquis@seloger.com>
2019-08-11 03:21:41 +0200

    Make Calorie request DTO fluent

--- src/main/java/opendigitaleducation/marquis/testtechnique/agregateurs/CalorieRequestDTO.java
+++ src/main/java/opendigitaleducation/marquis/testtechnique/agregateurs/CalorieRequestDTO.java
@@ -1,6 +1,6 @@
 package opendigitaleducation.marquis.testtechnique.agregateurs;
 
-@SuppressWarnings({"unused", "WeakerAccess"}) //JSon serialisation
+@SuppressWarnings({"unused", "WeakerAccess", "UnusedReturnValue"}) //JSon serialisation, Fluent
 public class CalorieRequestDTO {
   private String searchedAlimentName;
   private CalorieAgregateurType calorieAgregateurType;
@@ -9,15 +9,17 @@ public class CalorieRequestDTO {
     return calorieAgregateurType;
   }
 
-  public void setCalorieAgregateurType(CalorieAgregateurType calorieAgregateurType) {
+  public CalorieRequestDTO setCalorieAgregateurType(CalorieAgregateurType calorieAgregateurType) {
     this.calorieAgregateurType = calorieAgregateurType;
+    return this;
   }
 
   public String getSearchedAlimentName() {
     return searchedAlimentName;
   }
 
-  public void setSearchedAlimentName(String searchedAlimentName) {
+  public CalorieRequestDTO setSearchedAlimentName(String searchedAlimentName) {
     this.searchedAlimentName = searchedAlimentName;
+    return this;
   }
 }
